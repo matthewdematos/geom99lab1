4f56f31fb8bbdbac054142cfb3f4bec1510f6ce6
matthewdematos <146375117+matthewdematos@users.noreply.github.com>
2024-01-26 05:21:14 -0500

    Update mystory.js

--- story/mystory.js
+++ story/mystory.js
@@ -4,34 +4,13 @@ function initMap() {
     zoom: 3.5,
   });
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const locations = [
-    { lat: 44.515887, lng: -80.018285},
-    { lat: 46.369597, lng: -82.669177},
-    { lat: 43.7094533, lng: -79.7256149},
-    { lat: 43.7405, lng: -79.4246224},
-    { lat: 43.87663, lng: -79.4372},
-    { lat: 43.923742, lng: -79.453232},
-    { lat: 44.05217, lng: -79.460616},
-    { lat: 51.37166, lng: 1.134472},
-    { lat: 51.27309, lng: 1.06836},
-    { lat: 44.3662912, lng: -79.6815738}
-  ];
-        
-  //const placeInfo = ["1994: Wasaga Beach is where I spent the first 5 years of my life, blissfully unaware of the party culture I was surrounded by. My mom decided this was not a good place to raise kids.", 
-    "1999: Elliot Lake is where my grandma lived, and I spent just under a year here. Even at such a young age, I was taken by the beauty of the lakes and forests of the Canadian Shield."
-    ];
 
-  // for (let i = 0; i < placeInfo.length; ++i) {
-    const markers = locations.map((locations, i) => {
-      return new google.maps.Marker({
-        position: location,
-        label: labels[i % labels.length],
-        map: map,
-  // }
-    });
-                
-  //addPlaceInfo(markers, placeInfo[i]);
-  }
+  const markers = locations.map((locations, i) => {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    }
+  });
         
   new MarkerClusterer(map, markers, {
     imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",            
@@ -76,14 +55,18 @@ function animateArrow(lifePath) {
   }, 20);
 }
 
-//function addPlaceInfo(markers, placeInfo) {
-//  const infowindow = new google.maps.InfoWindow({
-//    content: placeInfo,
-//  });
-//  marker.addListener("click", () => {
-//    infowindow.open(marker.get("map"), marker);
-//  });
-//}
+  const locations = [
+    { lat: 44.515887, lng: -80.018285},
+    { lat: 46.369597, lng: -82.669177},
+    { lat: 43.7094533, lng: -79.7256149},
+    { lat: 43.7405, lng: -79.4246224},
+    { lat: 43.87663, lng: -79.4372},
+    { lat: 43.923742, lng: -79.453232},
+    { lat: 44.05217, lng: -79.460616},
+    { lat: 51.37166, lng: 1.134472},
+    { lat: 51.27309, lng: 1.06836},
+    { lat: 44.3662912, lng: -79.6815738}
+  ];
 
 window.initMap = initMap;
       
