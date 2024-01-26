b0f8c6e9402a4e8de94033b7b9e16d21cfff994d
matthewdematos <146375117+matthewdematos@users.noreply.github.com>
2024-01-26 04:27:53 -0500

    Update mystory.js

--- story/mystory.js
+++ story/mystory.js
@@ -9,28 +9,27 @@ function initMap() {
           // create an array of markers based on a given "locations" array.
           // The map() method here has nothing to do with the Google Maps API.
 
-        const contentString = ["<p><b>1994:</b>Wasaga Beach is where I spent the first 5 years of my life, blissfully unaware of the party culture I was surrounded by. " +
-             "My mom decided this was not a good place to raise kids.</p>", 
+        const contentString = ["1994: Wasaga Beach is where I spent the first 5 years of my life, blissfully unaware of the party culture I was surrounded by. " +
+             "My mom decided this was not a good place to raise kids.", 
             "<p><b>1999:</b>Elliot Lake is where my grandma lived, and I spent just under a year here. Even at such a young age, I was taken by the beauty of the " +
             "lakes and forests of the Canadian Shield."
              ];
         
-        const infowindow = new google.maps.InfoWindow({
-            content: contentString[i % contentString.length],
-          });
+        const infowindow = new google.maps.InfoWindow();
         
         const markers = locations.map((location, i) => {
           return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length],
           });
+
+        markers.addListener("click", (function(markers, i) => {
+           return function () { 
+           infowindow.setContent(contentString[i]);
+           infowindow.open(map, marker);
+            };
+        })(markers, i));
                 
-        markers.addListener("click", () => {
-            infowindow.open({
-              anchor: markers[i % markers.length],
-              map,
-            });
-        });
           // Add a marker clusterer to manage the markers.
         new MarkerClusterer(map, markers, {
           imagePath:
